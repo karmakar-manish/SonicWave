51474d1a5fb3c23fe2ae09ba0a1c0f95c1724919
karmakar-manish <manishkarmakar106@gmail.com>
2025-09-05 20:57:34 +0530

    NothingChanged

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://sonicwave-8g06.onrender.com"
-export const BACKEND_URL = "http://localhost:3000"
+export const BACKEND_URL = "https://sonicwave-8g06.onrender.com"
+// export const BACKEND_URL = "http://localhost:3000"
